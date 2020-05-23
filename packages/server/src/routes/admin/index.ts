eac37a84fa7a2402419602176ea0ef30aadd3426
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-05-23 19:52:08 +0200

    REMOVE unused files

--- packages/server/src/routes/admin/index.ts
+++
@@ -1,3 +0,0 @@
-export default [
-  import('~routes/admin/login'),
-];
--- packages/server/src/routes/admin/login.ts
+++
@@ -1,9 +0,0 @@
-import type { Express } from 'express';
-
-export default async function login(server: Express): Promise<void> {
-  server.get('/', (req, res) => {
-    res.json({
-      ichglaub: 'eshackt',
-    });
-  });
-}
